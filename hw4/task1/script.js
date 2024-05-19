cc7d1686f71b02e926550faeb86aa303471a0ea9
roman lepihov <lepihovrom@yandex.by>
2024-05-19 11:52:59 +0300

    add hw4 task1

---
+++ hw4/task1/script.js
@@ -0,0 +1,43 @@
+const url1 = 'https://jsonplaceholder.typicode.com/users';
+
+const getData = async (url) => {
+  try {
+    const res = await fetch(url);
+    return await res.json();
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+const mainSectionEl = document.querySelector('.mainSection');
+const userContainerTemplateEl = document.querySelector('#userTemplate');
+
+const fetchData = await getData(url1);
+
+fetchData.forEach(
+  function (el) {
+    mainSectionEl.appendChild(getCompletedTemplate(el));
+  }
+)
+
+function getCompletedTemplate(userData) {
+  const userContainerEl = userContainerTemplateEl.content.cloneNode(true);
+
+  userContainerEl.querySelector(".userInfo__name").textContent = userData.name;
+  userContainerEl.querySelector(".userInfo__username").textContent = userData.username;
+  userContainerEl.querySelector(".userInfo__phone").textContent = userData.name;
+  userContainerEl.querySelector(".userInfo__website").textContent = userData.name;
+
+  userContainerEl.querySelector(".address__street").textContent = userData.address.street;
+  userContainerEl.querySelector(".address__suite").textContent = userData.address.suite;
+  userContainerEl.querySelector(".address__city").textContent = userData.address.city;
+  userContainerEl.querySelector(".address__zipcode").textContent = userData.address.zipcode;
+  userContainerEl.querySelector(".address__lat").textContent = userData.address.geo.lat;
+  userContainerEl.querySelector(".address__lng").textContent = userData.address.geo.lng;
+
+  userContainerEl.querySelector(".company__name").textContent = userData.company.name;
+  userContainerEl.querySelector(".company__catchPhrase").textContent = userData.company.catchPhrase;
+  userContainerEl.querySelector(".company__bs").textContent = userData.company.bs;
+
+  return userContainerEl;
+}
